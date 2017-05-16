f1a512740682eabba1367b71b0b93ca66c88dccb
hetong007 <hetong007@gmail.com>
2017-05-15 17:33:25 -0700

    automate init.c

--- src/init.c
+++ src/init.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
 /* FIXME: 
@@ -6,18 +6,18 @@
 */
 
 /* .C calls */
-extern void CWrapper_encoding_isbig5(char **, int *);
-extern void CWrapper_encoding_isgbk(char **, int *);
-extern void CWrapper_encoding_isgb18030(char **, int *);
-extern void CWrapper_encoding_isgb2312(char **, int *);
-extern void CWrapper_encoding_isutf8(char **, int *);
+extern void CWrapper_encoding_isbig5(void *, void *);
+extern void CWrapper_encoding_isgb18030(void *, void *);
+extern void CWrapper_encoding_isgb2312(void *, void *);
+extern void CWrapper_encoding_isgbk(void *, void *);
+extern void CWrapper_encoding_isutf8(void *, void *);
 
 static const R_CMethodDef CEntries[] = {
-    {"CWrapper_encoding_isbig5", (DL_FUNC) &CWrapper_encoding_isbig5, 2},
-    {"CWrapper_encoding_isgbk", (DL_FUNC) &CWrapper_encoding_isgbk, 2},
+    {"CWrapper_encoding_isbig5",    (DL_FUNC) &CWrapper_encoding_isbig5,    2},
     {"CWrapper_encoding_isgb18030", (DL_FUNC) &CWrapper_encoding_isgb18030, 2},
-    {"CWrapper_encoding_isgb2312", (DL_FUNC) &CWrapper_encoding_isgb2312, 2},
-    {"CWrapper_encoding_isutf8", (DL_FUNC) &CWrapper_encoding_isutf8, 2},
+    {"CWrapper_encoding_isgb2312",  (DL_FUNC) &CWrapper_encoding_isgb2312,  2},
+    {"CWrapper_encoding_isgbk",     (DL_FUNC) &CWrapper_encoding_isgbk,     2},
+    {"CWrapper_encoding_isutf8",    (DL_FUNC) &CWrapper_encoding_isutf8,    2},
     {NULL, NULL, 0}
 };
 
